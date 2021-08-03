4cba18ce0c334ecf1140cc68199e5bd7aae19048
bgchoi205 <choibg205@naver.com>
2021-08-03 23:18:17 +0900

    회원 도메인, 리포지토리 생성

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,33 @@ public class HelloController {
     public String helloMvc( @RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // html이 아니라 http 화면의 body 상에 직접 데이터를 넣어준다.
+    public String helloStirng(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+
+    // api 방식(json으로 나온다.)
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
